4781582cb48e43252132fbf4d425cc243dafd6b8
ywCN <wangyouhao90@gmail.com>
2018-03-24 17:10:49 -0400

    dashboard

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+// import EntryList from './EntryList';
+
+const Dashboard = () => {
+  return (
+    <div>
+      {/* <EntryList /> */}
+      hey I am dashboard
+    </div>
+  );
+};
+
+export default Dashboard;
