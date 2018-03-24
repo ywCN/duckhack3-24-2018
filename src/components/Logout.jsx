9f19835ef4f6465260014fa6a9936169dd7a1755
ywCN <wangyouhao90@gmail.com>
2018-03-24 17:24:41 -0400

    build logout page

---
+++ src/components/Logout.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import GoogLogin from './GoogLogin';
+
+const Logout = () => {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>Thank you for using Budget Manager!</h1>
+      <GoogLogin />
+    </div>
+  );
+};
+
+export default Logout;
