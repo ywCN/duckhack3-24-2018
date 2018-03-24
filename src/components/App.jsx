a81f7bf6ec5137f15895dae8ff43abc6fc07b0a1
ywCN <wangyouhao90@gmail.com>
2018-03-24 17:25:17 -0400

    use components to build app pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { Container } from 'semantic-ui-react';
+
+import AppHeader from './AppHeader';
+import LandingPage from './LandingPage';
+import Dashboard from './Dashboard';
+import Logout from './Logout';
 
 class App extends Component {
   render() {
-    return <div>Hello World</div>;
+    return (
+      <BrowserRouter>
+        <Container style={{ marginTop: '3em' }}>
+          <AppHeader />
+          <Route exact path="/" component={LandingPage} />
+          <Route exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/logout" component={Logout} />
+        </Container>
+      </BrowserRouter>
+    );
   }
 }
 
