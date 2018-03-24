99c73d7347c436c955fd349fe82c52bd888021ee
ywCN <wangyouhao90@gmail.com>
2018-03-24 17:24:32 -0400

    update text on landing page

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -5,7 +5,8 @@ import GoogLogin from './GoogLogin';
 const Landing = () => {
   return (
     <div style={{ textAlign: 'center' }}>
-      <h1>Log your spending. Control your budget.</h1>
+      <h1>Log your spending.</h1>
+      <h1>Control your budget.</h1>
       <GoogLogin />
     </div>
   );
