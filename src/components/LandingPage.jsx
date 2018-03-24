66e2ecf76286efa9c1a8b30d9c87fe849a13b837
ywCN <wangyouhao90@gmail.com>
2018-03-24 17:43:06 -0400

    build login fail warning

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import GoogLogin from './GoogLogin';
 
-const Landing = () => {
-  return (
-    <div style={{ textAlign: 'center' }}>
-      <h1>Manage your spending.</h1>
-      <h1>Control your budget.</h1>
-      <GoogLogin />
-    </div>
-  );
-};
+class LandingPage extends Component {
+  render() {
+    console.log(this.props.loginStatus);
+    console.log(this.props.loginStatus);
+    console.log(this.props.loginStatus);
+    const loginFailWarning =
+      this.props.loginStatus === false ? (
+        <h1>Login failed. Please try again.</h1>
+      ) : null;
+    return (
+      <div style={{ textAlign: 'center' }}>
+        <h1>Manage your spending.</h1>
+        <h1>Control your budget.</h1>
+        <GoogLogin />
+        {loginFailWarning}
+      </div>
+    );
+  }
+}
 
-export default Landing;
+function mapStateToProps(state) {
+  return { loginStatus: state.user.loginStatus };
+}
+
+export default connect(mapStateToProps)(LandingPage);
