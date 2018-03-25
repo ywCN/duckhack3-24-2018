032cde2753008be1b54b5bd28aded1d4c44f49e4
ywCN <wangyouhao90@gmail.com>
2018-03-24 20:31:29 -0400

    init firebase

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import firebase from 'firebase';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { Container } from 'semantic-ui-react';
 
@@ -8,6 +9,18 @@ import Dashboard from './Dashboard';
 import Logout from './Logout';
 
 class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyCDpJCuM6xGFs7J9mKlMc_e8UV0O_90mBY',
+      authDomain: 'duckhack-d220e.firebaseapp.com',
+      databaseURL: 'https://duckhack-d220e.firebaseio.com',
+      projectId: 'duckhack-d220e',
+      storageBucket: '',
+      messagingSenderId: '235498252338'
+    };
+
+    firebase.initializeApp(config);
+  }
   render() {
     return (
       <BrowserRouter>
